0f5d27d90cb26f60ee47a32eb4d05e829836ed1c
Daen05 <144191197+Daen05@users.noreply.github.com>
2023-09-06 18:24:29 -0600

    Add files via upload
    Ceballos Calatayud Jefry Daen
    1S22
    Practica 1

---
+++ p1_1s22.java
@@ -0,0 +1,10 @@
+class p1_1s22{
+	public static void main(String[] args) {
+		System.out.print("Jeffrey Daen Ceballos Calatayud");
+		System.out.print("19 años");
+				System.out.print("jceballos13435-28@ccopm.edu.mx");
+		System.out.print("Estar en el gimnasio");
+		System.out.print("Rojo y negro");
+		
+	}
+}
