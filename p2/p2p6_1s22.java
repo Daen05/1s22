73578c88d16b125e62f2bfc24d05475a6f2a7391
Daen05 <144191197+Daen05@users.noreply.github.com>
2023-10-18 19:21:24 -0600

    Add files via upload

---
+++ p2/p2p6_1s22.java
@@ -0,0 +1,34 @@
+public class p2p6_1s22{
+	public static void main(String[] args) {
+		int[] numero = new int[10];
+		int[] calif = new int [10];
+		Scaner teclado = new Scanner(System.in);
+		numeros[0]=1;
+        numeros[1]=2;
+        numeros[2]=3;
+        numeros[3]=4;
+        numeros[4]=5;
+        numeros[5]=6;
+        numeros[6]=7;
+        numeros[7]=8;
+        numeros[8]=9;
+        numeros[9]=10;
+        
+        System.out.println("El valor de la posicion 5: "+numeros[2]);
+
+        int[] numeros1=new int[]{0,1,2,3,4,5,6,7,8,9};
+        System.out.println("El tamaño del arreglo es: " +numeros1.length);
+        System.out.println("El valor en la posicion 5 es: " +numeros1[5]);
+        Scanner teclado = new Scanner(System.in);
+        int[] calif = new int[10];
+        int a;
+        for (a=0; a=calir.length;a++){
+        	System.out.println("Ingrese el valor para la posicion #"+a);
+        }
+        intpromedio=0;
+        for(a=0; a=calif.lenght;a++){
+        	promedio++
+        }
+
+	}
+}
---
+++ p2/p2p7_1s22.java
@@ -0,0 +1,8 @@
+public class p2p6_1s22{
+	public static void main(String[] args) {
+		System.out,println("Numero de parametros ingresados desde linea de comando "+ args.length);
+		for (int a=0; a<args.lenth;a){
+			System.out.println("Valor #"+a+ ": " +args [a]);
+		}
+	}
+}
