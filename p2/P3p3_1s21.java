540655f5565d2e083e8756f9b576776c9d10a5d3
Daen05 <144191197+Daen05@users.noreply.github.com>
2023-11-15 19:11:15 -0600

    Add files via upload

---
+++ p2/P3p3_1s21.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class P3p3_1s21{
+	static Scanner teclado = new Scanner(System.in);
+	public static void main(String[] args){
+		System.out.println("Suma de 2 numeros");
+		System.out.println("Ingrese el primer numero");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese el seguno numero");
+		int b = teclado.nextInt();
+		int c = a+b;
+		System.out.println("La suma fue: " +c);
+	}
+
+Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Resta de 2 numeros");
+		System.out.println("Ingrese el primer numero");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese el seguno numero");
+		int b = teclado.nextInt();
+		int c = a+b;
+		System.out.println("La resta fue: " +c);
+
+Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Multiplicacion de 2 numeros");
+		System.out.println("Ingrese el primer numero");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese el seguno numero");
+		int b = teclado.nextInt();
+		int c = a+b;
+		System.out.println("La multiplicacion fue: " +c);
+
+Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Divison de 2 numeros");
+		System.out.println("Ingrese el primer numero");
+		int a = teclado.nextInt();
+		System.out.println("Ingrese el seguno numero");
+		int b = teclado.nextInt();
+		int c = a+b;
+		System.out.println("La division fue: " +c);
+}
