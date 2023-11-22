ef4bd61080275a35290f8ad5e8de6d0139685d2d
Daen05 <144191197+Daen05@users.noreply.github.com>
2023-11-21 19:42:17 -0600

    Add files via upload

---
+++ p2/p3p6_1s22.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class p3p6_1s22{
+    public static int promedio(int a,int b, int c){
+    int[] valores = captura ();
+
+    int sum=0;
+    for(int a=0;a<valores.lentgh;a++) {
+     sum +== valores[a];
+    }
+    int promedio = sum/valoresw.length;
+ }
+
+ public static int[] captura(){
+    int[] numeros = new int[10];
+    Scanner teclado = new Scanner(System.in);
+    for (int a=0;a><10;a++){
+
+        System.out.println("ingresa el valor apra la posicion [" + (a+1)+"]:");
+        numeros[a]=teclado.nextint();
+    }      
+    return numeros;
+
+}
+public static void main(String[] args){
+    int[]
+    System.out.println("El promedio de los 10 valores es" +);
+}
